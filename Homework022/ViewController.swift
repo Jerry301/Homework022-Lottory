2e3baac8cb42b4f7dffadc88dd3857dacbcdf794
Jerry Lin <cylin520@ntu.edu.tw>
2021-12-23 15:48:02 +0800

    Initial Commit

---
+++ Homework022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework022
+//
+//  Created by Chun-Yi Lin on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
